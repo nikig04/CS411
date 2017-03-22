1994a887a81254ea2ff4cb08a1db2d6d7bf10fb8
Dimitri <dmak1112@bu.edu>
2017-03-22 01:13:23 -0400

    removed extraneous projects that were used for testing purposes

--- Django/storytime/story/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render_to_response
-from django.http import HttpResponse
-from .models import Line
-def home(request):
-	return render_to_response("story/home.html", {'lines': Line.objects.all()}) 
-
-# Create your views here.
