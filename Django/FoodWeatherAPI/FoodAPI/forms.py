f4a2b389f2dc859680377503384f22be9872edb1
mfyc <mcsmocha@bu.edu>
2017-04-26 18:12:14 -0400

    fixed feather b/w logo and password1 help_text

--- Django/FoodWeatherAPI/FoodAPI/forms.py
+++ Django/FoodWeatherAPI/FoodAPI/forms.py
@@ -4,9 +4,10 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    first_name = forms.CharField(max_length=30, required=False, help_text='Required')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Required')
     email = forms.EmailField(max_length=254, help_text='Required. Input a valid email address.')
+    password1 = forms.CharField(label= ("Password"), widget=forms.PasswordInput, help_text = ("Must contain at least 8 characters."))
 
     class Meta:
         model = User
